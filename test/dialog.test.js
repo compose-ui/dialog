45ac4e0c4b15dbbf581d202e8d339cc4c8632104
Jerome Gravel-Niquet <jeromegn@gmail.com>
2014-07-23 18:29:21 -0400

    fixed the tests

--- test/dialog.test.js
+++ test/dialog.test.js
@@ -4,15 +4,10 @@ var domify = require('domify')
 var bean = require('bean')
 
 describe('Dialog', function(){
-  describe('automatic instantiation', function(){
-    before(function(){
-      var a = domify('<a href="#" data-trigger="dialog"></a>')
-      a.setAttribute('data-title', 'This is a title')
-      document.body.appendChild(a)
-      bean.fire(a, 'click')
-    })
-    it('shows a dialog box with the proper data', function(){
-      assert.equal(document.querySelector('.dialog h2').textContent, 'This is a title')
-    })
+  before(function(){
+    new Dialog({title: 'This is a title'}).show()
+  })
+  it('shows a dialog box with the proper data', function(){
+    assert.equal(document.querySelector('.dialog h2').textContent, 'This is a title')
   })
 })
